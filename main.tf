5859f6ee3544cc20d1b483ddff7e563ab29dd739
Hariom Kumar <hariomkumardev8920@gmail.com>
2026-08-17 12:31:49 +0530

    new yml code update kiya hai

--- main.tf
+++ main.tf
@@ -68,39 +68,3 @@ resource "azurerm_network_interface" "frontend_nic" {
 }
 
 
-resource "azurerm_virtual_machine" "frontend_vm_check" {
-  name                  = "frontend-vm"
-  location              = azurerm_resource_group.rg.location
-  resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = [azurerm_network_interface.frontend_nic.id]
-  vm_size               = "Standard_B2ms"
-
-  storage_os_disk {
-    name              = "frontend-vm-osdisk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
-  }
-
-  os_profile {
-    computer_name  = "frontend-vm"
-    admin_username = "azureadmin"
-    admin_password = "ChangeMe123!StrongPassword"
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-
-  tags = {
-    environment = "prod"
-    managed_by  = "terraform"
-  }
-}
